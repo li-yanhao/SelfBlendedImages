c8838c5ec405d8b771f2acacd0ee72809f9b494f
li-yanhao <yanhao.li@outlook.com>
2022-09-27 16:20:57 +0200

    update

--- run.sh
+++ run.sh
@@ -21,9 +21,14 @@ cd $bin
 # pwd
 # ls
 
-python src/inference/inference_image.py -w weights/FFraw.tar -i $exec_root/input_0.png
+# python src/inference/inference_image.py -w weights/FFraw.tar -i $exec_root/input_0.png
     # >> $bin/stdout.txt
-
+    
+python src/inference/inference_image.py \
+    -w weights/FFraw.tar \
+    -e weights/adv-efficientnet-b4-44fb3a87.pth \
+    -r weights/retinaface_resnet50_2020-07-20.pth \
+    -i $exec_root/input_0.png
 
 # mv output.png $exec_root
 echo Successful!
